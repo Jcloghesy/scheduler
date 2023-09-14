b3471edccbb7274461bc6435a513eeb4456f9a9a
John <john.cloghesy@gmail.com>
2023-09-14 11:45:49 -0600

    Appointments: Displaying Appointments - Implement Display Show & Empty
    - Update the Appointment component to display the Show component with booking

--- src/components/Appointments/index.js
+++ src/components/Appointments/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import 'components/Appointment/styles.scss'
+import 'components/Appointment/styles.scss';
+import Header from 'components/Appointment/Header';
+import Show from 'components/Appointment/Show';
+import Empty from 'components/Appointment/Empty';
 
 export default function Appointment(props) {
+  const { time, interview } = props;
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={time} />
+      {interview ? <Show student={interview.student} interviewer={interview.interviewer} /> : <Empty />}
+    </article>
   )
 }
--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from 'react'
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -151,6 +151,22 @@ storiesOf("InterviewerListItem", module)
       <Appointment
         time= "12pm" 
       />
+    ))
+    .add("Appointment Empty", () => (
+      <Fragment>
+        <Appointment id={1} time="4pm" />
+        <Appointment time="5pm" />
+      </Fragment>
+    ))
+    .add("Appointment Booked", () => (
+      <Fragment>
+        <Appointment
+          id={1}
+          time="4pm"
+          interview={{ student: "Lydia Miller-Jones", interviewer }}
+        />
+        <Appointment time="5pm" />
+      </Fragment>
     ))
     .add("Header", () => (
       <Header
