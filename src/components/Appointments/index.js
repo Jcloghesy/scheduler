daca07016e1b11f730e10a13e64d6ddb9072e732
John <john.cloghesy@gmail.com>
2023-09-14 17:14:18 -0600

    Custom Hooks: Using Custom Hook - Integrate useVisualMode for internal nav
     - Establish internal navigation by replacing the existing logic  with useVisualMode hook 
     - Ensure useVisualMode determines  correct mode and renders matching component

--- src/components/Appointments/index.js
+++ src/components/Appointments/index.js
@@ -3,13 +3,36 @@ import 'components/Appointment/styles.scss';
 import Header from 'components/Appointment/Header';
 import Show from 'components/Appointment/Show';
 import Empty from 'components/Appointment/Empty';
+import Form from 'components/Appointment/Form';
+import useVisualMode from 'hooks/useVisualMode';
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+const interviewers = [];
 
 export default function Appointment(props) {
   const { time, interview } = props;
+  const { mode, transition, back } = useVisualMode(
+    interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
       <Header time={time} />
-      {interview ? <Show student={interview.student} interviewer={interview.interviewer} /> : <Empty />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={interview.student}
+          interviewer={interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={interviewers}
+          onCancel={() => back()}
+        />
+      )}
     </article>
   )
 }
